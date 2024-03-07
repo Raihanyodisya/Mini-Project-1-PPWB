b9050dabbafdbb461ce0e4d7dfa34fdf98a5f88a
Raihan <126870046+Raihanyodisya@users.noreply.github.com>
2024-03-07 22:55:17 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.getElementById('burger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('show-links');
+});
